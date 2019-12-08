77be7eada55ea82c9f1174a5a92c25d29ca1d24d
pdeng <635639654@qq.com>
2019-12-09 00:17:34 +0800

    创建 user 模型, 并实现 crud 操作

--- server/apps/admin/src/app.module.ts
+++ server/apps/admin/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { DbModule } from '@libs/db'
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DbModule,
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest Full Stack - 后台管理系统 API')
+    .setDescription('提供后台管理界面调用的服务端 API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000)
+  console.log(`http://www.localhost:3000/api-docs`)
 }
 bootstrap()
