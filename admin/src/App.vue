664d4a7549bfb8e7b2b24d20c87e94783b14a8e8
pdeng <635639654@qq.com>
2019-12-10 16:23:06 +0800

    vue + element + ts 创建后端管理界面

---
+++ admin/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <About />
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import About from './views/About.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    About
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
