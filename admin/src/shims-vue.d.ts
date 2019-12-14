0b6a6ecaee6659e9d9f5889da221c505fc862c73
pdeng <635639654@qq.com>
2019-12-15 00:28:53 +0800

    axios 支持ts

--- admin/src/shims-vue.d.ts
+++ admin/src/shims-vue.d.ts
@@ -3,10 +3,4 @@ declare module "*.vue" {
   export default Vue;
 }
 
-import Vue from 'vue'
-import { AxiosInstance } from 'axios'
-declare module 'vue/types/vue' {
-  interface Vue {
-    $http: AxiosInstance
-  }
-}
+s
